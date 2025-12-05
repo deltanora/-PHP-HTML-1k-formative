4b48c5667e1b42f19dac7b8acc029970de2d9ef6
Your Name <you@example.com>
2025-12-05 09:12:15 +0200

    paragraph added

--- about.php
+++ about.php
@@ -1,1 +1,32 @@
-<!-- your code goes here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php
+    echo "About";
+
+
+    ?>
+    </h1>
+<p>
+    <?php
+    $indexedArray = [
+        "Lorem ipsum dolor sit amet.",
+    "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto."
+];
+    foreach($indexedArray as $value){
+        echo ($value);
+
+    }
+
+
+    ?>
+    </p>
+</body>
+</html>
